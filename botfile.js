6a0291bb0a69b94e49a7dfe4b69798506419f86a
Kurt Johnson <fazbat@gmail.com>
2017-06-04 11:41:12 -0700

    adding postgress DB for config on Heroku

--- botfile.js
+++ botfile.js
@@ -40,7 +40,8 @@ module.exports = {
     user: process.env.PG_USER || '',
     password: process.env.PG_PASSWORD || '',
     database: process.env.PG_DB || '',
-    ssl: process.env.PG_SSL || false
+    ssl: process.env.PG_SSL || true,
+    connection: process.env.DATABASE_URL,
   },
 
   /**
